944db37aa53b3817ead60e3e17ef1798a9f44614
kovacpetar <kovac.petar997@gmail.com>
2024-05-11 02:57:42 -0700

    Add CreateCinema method
    - Finish task 3

--- Code9.API/Controllers/CinemaController.cs
+++ Code9.API/Controllers/CinemaController.cs
@@ -1,4 +1,6 @@
-﻿using Code9.Domain.Handlers.Queries;
+﻿using Code9.API.Models;
+using Code9.Domain.Handlers.Commands;
+using Code9.Domain.Handlers.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,10 +10,17 @@ namespace Code9.API.Controllers
     [Route("[controller]")]
     public class CinemaController(IMediator mediator) : ControllerBase
     {
-        [HttpGet()]
+        [HttpGet]
         public async Task<IResult> GetAll()
         {
             return Results.Ok(await mediator.Send(new GetAllCinemaQuery()));
+        }
+
+        [HttpPost]
+        public async Task<IResult> CreateCinema([FromBody] AddCinemaRequest request)
+        {
+            var command = new AddCinemaCommand(request.City, request.Name, request.Street, request.NumOfAuditoriums);
+            return Results.Ok(await mediator.Send(command));
         }
     }
 }
--- Code9.Domain/Handlers/Commands/AddCinemaCommand.cs
+++ Code9.Domain/Handlers/Commands/AddCinemaCommand.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Code9.Domain.Interfaces;
+using Code9.Domain.Models;
+using MediatR;
 
 namespace Code9.Domain.Handlers.Commands
 {
-    internal class AddCinemaCommand
+    public record AddCinemaCommand(
+        string City, 
+        string Name, 
+        string Street, 
+        int NumOfAuditoriums) : IRequest<Cinema>;
+
+    public class AddCinemaHandler : IRequestHandler<AddCinemaCommand, Cinema>
     {
+        private readonly ICinemaRepository cinemaRepository;
+
+        public AddCinemaHandler(ICinemaRepository cinemaRepository)
+        {
+            this.cinemaRepository = cinemaRepository;
+        }
+
+        public async Task<Cinema> Handle(AddCinemaCommand request, CancellationToken cancellationToken)
+        {
+            var cinemaDb = await cinemaRepository.AddCinema(new Cinema
+            {
+                City = request.City,
+                Name = request.Name,
+                Street = request.Street,
+                NumberOfAuditoriums = request.NumOfAuditoriums
+            });
+
+            return cinemaDb;
+        }
     }
 }
