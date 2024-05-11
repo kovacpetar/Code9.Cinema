ca599648d5f9ad6c29febec8356ea36d7dc88cfb
kovacpetar <kovac.petar997@gmail.com>
2024-05-11 03:23:09 -0700

    Implement Update logic

--- Code9.API/Controllers/CinemaController.cs
+++ Code9.API/Controllers/CinemaController.cs
@@ -2,6 +2,7 @@
 using Code9.Domain.Handlers.Commands;
 using Code9.Domain.Handlers.Queries;
 using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Code9.API.Controllers
@@ -21,6 +22,14 @@ namespace Code9.API.Controllers
         {
             var command = new AddCinemaCommand(request.City, request.Name, request.Street, request.NumOfAuditoriums);
             return Results.Ok(await mediator.Send(command));
+        }
+
+        [HttpPut]
+        public async Task<IResult> UpdateCinema([AsParameters] Guid Id, [FromBody] UpdateCinemaRequest request)
+        {
+            var command = new UpdateCinemaCommand(Id, request.City, request.Name, request.Street, request.NumOfAuditoriums);
+            await mediator.Send(command);
+            return Results.NoContent();
         }
     }
 }
